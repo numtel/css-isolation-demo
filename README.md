9b9558ae9f0356482412bf15b9ddbd1f22ab8cc1
numtel <bgreen787@gmail.com>
2014-10-24 02:14:00 -0700

    Added x-tag example, further cleanup

---
+++ README.md
@@ -0,0 +1,3 @@
+# CSS Isolation in Meteor Components Comparison
+
+X-tag is the shiz! [See the full details...](http://css-isolation.meteor.com/)
---
+++ client/xtagsExample.js
@@ -0,0 +1,50 @@
+
+var extractComment = function (f) {
+  return f.toString().split('/*')[1].split('*/')[0];
+};
+
+var templateHeader = extractComment(function () { /*
+  <style>
+    :host {
+      border: 1px inset;
+      background: #f5f5f5;
+      padding: 20px;
+      display: block;
+    }
+    em {
+      text-decoration: underline;
+    }
+  </style>
+*/ });
+
+
+xtag.register('template-component', {
+  lifecycle: {
+    created: function () {
+      this.shadowRoot = this.createShadowRoot();
+      this.uiSetTemplate();
+    },
+    attributeChanged: function () {
+      this.uiSetTemplate();
+    }
+  },
+  accessors: {
+    src: {
+      attribute: {},
+      get: function () {
+        return this.getAttribute('src');
+      },
+      set: function (value) {
+        this.xtag.data.src = value;
+      }
+    }
+  },
+  methods: {
+    uiSetTemplate: function () {
+      this.shadowRoot.innerHTML = templateHeader;
+      if(this.src){
+        Blaze.render(Template[this.src], this.shadowRoot);
+      }
+    }
+  }
+});
